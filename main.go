5297459c16d37885bfa86f4345f61b634d3fcc9a
tofuso <tofu.so.shioaji@gmail.com>
2018-09-26 04:10:21 +0900

    change system

--- main.go
+++ main.go
@@ -5,23 +5,32 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"os/exec"
 	"os/signal"
 	"unicode/utf8"
+
+	"github.com/tofuso/remipi/scancode"
 )
 
 var (
 	textMessage = flag.String("s", "Hello World!", "キーボードに出力させる文字を指定してください。")
-	binDir      = flag.String("d", "~/hardpass-sendHID", "scanバイナリが存在するディレクトリ。")
+	dir         = flag.String("d", "/dev/hidg0", "デバイスファイル")
 	talk        = flag.Bool("t", false, "指定すると対話モードで起動します。Ctr+Cで終了。")
+	devf        *os.File
 )
 
 func main() {
-	flag.Parse()
+	var err error
+	flag.Parse()              //引数をパース
+	devf, err = os.Open(*dir) //デバイスをオープン
+	if err != nil {
+		//デバイスを開く過程でエラーが発生
+		fmt.Println(err)
+		return
+	}
 	if !*talk {
 		//一回だけ実行
 		fmt.Println("入力された文字: ", *textMessage)
-		err := write(*textMessage)
+		err := run(*textMessage)
 		if err != nil {
 			fmt.Println(err)
 			return
@@ -35,7 +44,7 @@ func main() {
 		//シグナルを受信するまで実行
 		for scanner.Scan() {
 			if s := scanner.Text(); utf8.RuneCountInString(s) > 0 {
-				err := write(s)
+				err := run(s)
 				if err != nil {
 					fmt.Println(err)
 					return
@@ -50,10 +59,64 @@ func main() {
 	}
 }
 
-//キーボードに書き込む
-func write(str string) error {
-	l := "echo -n \"" + str + "\" | sudo " + *binDir + "/scan /dev/hidg0 1 2"
-	err := exec.Command("sh", "-c", l).Run()
-	//_, err := fmt.Println(l)
+//キーボードに書き込む（開放も行われる）
+func writekey(key scancode.Key) error {
+	_, err := fmt.Fprintf(devf, "\\%X\\0\\%X\\0\\0\\0\\0\\0", key.Top, key.ID)
+	if err != nil {
+		return err
+	}
+	//開放
+	_, err = fmt.Fprintf(devf, "\\%X\\0\\%X\\0\\0\\0\\0\\0", scancode.Open.Top, scancode.Open.ID)
 	return err
+}
+
+//解析し、キーに打ち込む処理を行う
+func run(s string) error {
+	actf := false   //コマンド中か判定するフラグ
+	var acts string //コマンドの内容を保存する
+	for _, r := range s {
+		if r == '|' && !actf {
+			//コマンド開始
+			actf = true //フラグを立てる
+			acts = ""   //初期化
+		} else if r == '|' && actf {
+			//コマンド終了
+			if actkey, ok := scancode.ActionMap[acts]; ok {
+				//コマンドあり
+				err := writekey(actkey)
+				if err != nil {
+					return err
+				}
+			} else {
+				//該当コマンドなし
+				fmt.Println("該当するコマンドがありませんでした。: ", acts)
+			}
+		} else if actf {
+			//コマンド取得中
+			acts += string(r)
+		} else if key, ok := scancode.KeyMap[r]; ok {
+			//通常のKeyであるなら
+			err := writekey(key)
+			if err != nil {
+				return err
+			}
+		} else if skey, ok := scancode.JapaneaseKeyMap[r]; ok {
+			//ひらがなのKeyであるなら日本語入力モードにする
+			err := writekey(scancode.ChgIn)
+			if err != nil {
+				return err
+			}
+			//入力
+			for _, k := range skey {
+				err = writekey(k)
+				if err != nil {
+					return err
+				}
+			}
+		} else {
+			//該当する文字がない時
+			fmt.Println("該当する文字がありません: ", r)
+		}
+	}
+	return nil
 }
